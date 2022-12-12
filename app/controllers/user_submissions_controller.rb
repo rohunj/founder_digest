0e714f46493964c30dfd5e08878786278c11e346
Rohunj <rohunjauhar@gmail.com>
2022-12-12 14:34:18 -0600

    migration to store intended subscripton plan and UserSubmission validations

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -1,12 +1,13 @@
 class UserSubmissionsController < ApplicationController
   def create
-    puts "WHITELISTED PARAMS: #{user_submission_params}"
     UserSubmission.create!(user_submission_params)
+    #TODO: redirect to a thank you page
+    redirect_to root_path, notice: 'Thanks! Your application was received'
   #in here, we will accept the client reuqeest (form data) and
   # create a new user submission record
   end
 
   def user_submission_params
-    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :text, :job_role)
+    params.require(:user_submission).permit(:first_name, :last_name, :email, :website, :text, :job_role, :plan_name)
   end
 end
